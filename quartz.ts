5bceb283063c24d6789eace4b120320dd867e3b8
Moose-DM <zalseika14@gmail.com>
2026-08-10 19:56:24 -0400

    Quartz sync: Aug 10, 2026, 7:56 PM

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,15 @@
+import * as ExternalPlugin from "./.quartz/plugins"
+import { customImage } from "./quartz/components/og" // Ensure export name matches your file
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+
+// Must be called BEFORE loadQuartzConfig()
+ExternalPlugin.CustomOgImages({
+  colorScheme: "darkMode",
+  width: 1200,
+  height: 630,
+  excludeRoot: false,
+  imageStructure: customImage,
+})
 
 const config = await loadQuartzConfig()
 export default config
